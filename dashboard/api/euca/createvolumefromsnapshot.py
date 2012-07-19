c9ba56b843373f985c9bd7cf7097c03322279203
I H Shaon <mdshaonimran@gmail.com>
2012-07-20 03:11:07 +0600

    improved design and many fixes

---
+++ dashboard/api/euca/createvolumefromsnapshot.py
@@ -0,0 +1,47 @@
+# Software License Agreement (BSD License)
+#
+# Redistribution and use of this software in source and binary forms, with or
+# without modification, are permitted provided that the following conditions
+# are met:
+#
+#   Redistributions of source code must retain the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other
+#   materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Imran Hossain Shaon mdshaonimran@gmail.com
+
+# file: dashboard/api/euca/createvolume.py
+
+import dashboard.api.nilgiricommand
+from boto.roboto.param import Param
+
+class CreateVolumeFromSnapshot(dashboard.api.nilgiricommand.NilgiriCommand):
+
+    def main(self, userid, snapshot_id, vol_zone):
+        conn = self.make_connection_cli(userid)
+        return conn.create_volume(snapshot=snapshot_id, zone=vol_zone, size="")
+
+    def main_cli(self, userid, snapshot_id, vol_zone):
+        volume = self.main(userid, snapshot_id, vol_zone)
+        if volume:
+            return volume
+        else:
+            return "Failed"
+
